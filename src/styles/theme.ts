d1c6bad88e95d1be6ca898a45076442b3f7dbbab
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-20 09:49:47 -0300

    Migração de meuPrimeiroApp para marcacaoDeConsultasMedicas

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export default {
+    colors: {
+      primary: '#EEDAE5',
+      secondary: '#00C48C',
+      background: '#F5F5F5',
+      text: '#333333',
+      error: '#FF647C',
+      success: '#00C48C',
+      white: '#FFFFFF',
+      vinho: '#8A4B56'
+    },
+    typography: {
+      title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+      },
+      subtitle: {
+        fontSize: 18,
+        fontWeight: '500',
+      },
+      body: {
+        fontSize: 16,
+      },
+    },
+    spacing: {
+      small: 8,
+      medium: 16,
+      large: 24,
+    },
+  };
